596985d7eecf7c5f1d92ea172730ba94a235eaf3
Long Truong Thanh Vo <longspin0220@gmail.com>
2026-08-11 11:54:21 +0700

    fix: load PNG volleyball asset for poster 01

--- server/VolleyDraft.Api/Services/Posters/CourtIndexAssetPosterRenderer.cs
+++ server/VolleyDraft.Api/Services/Posters/CourtIndexAssetPosterRenderer.cs
@@ -9,7 +9,7 @@ namespace VolleyDraft.Api.Services.Posters;
 public static class CourtIndexAssetPosterRenderer
 {
     private const string VolleyballResourceName =
-        "VolleyDraft.Api.Assets.Posters.CourtIndexVolleyballStudy02.jpg";
+        "VolleyDraft.Api.Assets.Posters.CourtIndexVolleyballStudy02.png";
 
     private static readonly SKRect VolleyballDestination =
         new(88, 1096, 543, 1526);
