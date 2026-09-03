8917d5840f219c822df54ddfe8e6814c28c67929
Long Truong Thanh Vo <longspin0220@gmail.com>
2026-09-04 06:16:08 +0700

    test: make Vietnam weekday fixtures boundary-testable

--- server/VolleyDraft.Api.Tests/Zalo/ZaloTestDates.cs
+++ server/VolleyDraft.Api.Tests/Zalo/ZaloTestDates.cs
@@ -4,9 +4,18 @@ internal static class ZaloTestDates
 {
     private static readonly TimeSpan VietnamOffset = TimeSpan.FromHours(7);
 
-    public static DateTimeOffset Next(DayOfWeek dayOfWeek, int hour = 19)
+    public static DateTimeOffset Next(DayOfWeek dayOfWeek, int hour = 19) =>
+        Next(dayOfWeek, DateTimeOffset.UtcNow, hour);
+
+    internal static DateTimeOffset Next(
+        DayOfWeek dayOfWeek,
+        DateTimeOffset referenceTime,
+        int hour = 19)
     {
-        var now = DateTimeOffset.UtcNow.ToOffset(VietnamOffset);
+        if (hour is < 0 or > 23)
+            throw new ArgumentOutOfRangeException(nameof(hour));
+
+        var now = referenceTime.ToOffset(VietnamOffset);
         var daysAhead = ((int)dayOfWeek - (int)now.DayOfWeek + 7) % 7;
         if (daysAhead == 0)
             daysAhead = 7;
